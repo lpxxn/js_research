169f816ea634fe0e8084f4e91e8eeef5dc4277d6
Aiden <mi_duo@live.com>
2022-09-07 23:37:36 +0800

    feat: test

---
+++ tsc_research/hello/base_value.ts
@@ -0,0 +1,16 @@
+var str = '1';
+
+/*
+类型断言（Type Assertion）
+类型断言可以用来手动指定一个值的类型，即允许变量从一种类型更改为另一种类型。
+语法格式：
+<类型>值
+或:
+值 as 类型
+ */
+//var str2: number = <number> <any> str;
+var str2: number = <number> <any> str;
+console.log(str2);
+
+var str3: number = str as any as number;
+console.log(str3);
