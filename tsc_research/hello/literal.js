4f510654fae6f7530bd6c576b5b19b553c583a55
Aiden <mi_duo@live.com>
2022-09-18 10:23:27 +0800

    feat: test

--- tsc_research/hello/literal.js
+++ tsc_research/hello/literal.js
@@ -1,3 +1,11 @@
 var add = function (a, b) { return a + b; };
 var x = add(1, 2);
 console.log(x);
+var a = "abc";
+var b = "abc";
+console.log(a, b);
+function move(dir) {
+    console.log(dir);
+}
+move("UP");
+//move("aaa");// err
--- tsc_research/hello/literal.ts
+++ tsc_research/hello/literal.ts
@@ -4,3 +4,16 @@ const add: Adder = (a, b) => a + b;
 
 const x = add(1, 2);
 console.log(x);
+
+let a = "abc";
+const b = "abc";
+console.log(a, b);
+
+type Direction = 'UP'|'DOWN';
+
+function move(dir: Direction) {
+    console.log(dir);
+}
+
+move("UP");
+//move("aaa");// err
