ef14e16824235e2c60cfcae3560787648089f61d
Aiden <mi_duo@live.com>
2022-09-18 21:33:49 +0800

    feat: test

--- tsc_research/hello/literal.js
+++ tsc_research/hello/literal.js
@@ -1,3 +1,4 @@
+var _this = this;
 var add = function (a, b) { return a + b; };
 var x = add(1, 2);
 console.log(x);
@@ -8,4 +9,12 @@ function move(dir) {
     console.log(dir);
 }
 move("UP");
-//move("aaa");// err
+var i1 = {
+    size: "small",
+    isEnable: true,
+    margin: 1,
+    getSize: function () {
+        return _this.size;
+    }
+};
+console.log(i1.isEnable, i1.getSize());
--- tsc_research/hello/literal.ts
+++ tsc_research/hello/literal.ts
@@ -9,11 +9,29 @@ let a = "abc";
 const b = "abc";
 console.log(a, b);
 
-type Direction = 'UP'|'DOWN';
+type Direction = 'UP' | 'DOWN';
 
 function move(dir: Direction) {
     console.log(dir);
 }
 
 move("UP");
+
 //move("aaa");// err
+
+interface Config {
+    size: 'small' | 'big';
+    isEnable: true | false
+    margin: 0 | 1 | 2;
+    getSize: (this) => string;
+}
+
+let i1: Config = {
+    size: "small",
+    isEnable: true,
+    margin: 1,
+    getSize: (): string => {
+        return this.size;
+    }
+}
+console.log(i1.isEnable, i1.getSize());
