030249958ac5844a710292887bed70b75c82c0bb
Aiden <mi_duo@live.com>
2022-09-26 10:42:55 +0800

    feat: eslink

---
+++ javascript_demo/eslintDemo/demo1/index.js
@@ -0,0 +1,12 @@
+//  npx eslint index.js
+//for(let i = 0; i< 10; i--) {
+for (let i = 0; i < 10; i++) {
+    console.log(1);
+}
+
+var obj = {x: 1, y: 2, z: "abc"};
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var x = obj.x;
+var a1 = arr[0];
+console.log(x, a1);
--- javascript_demo/eslintDemo/demo1/readme.md
+++ javascript_demo/eslintDemo/demo1/readme.md
@@ -4,3 +4,9 @@ npm init -y
 
 npm init @eslint/config
 check syntax and find problems
+
+
+https://www.bilibili.com/video/BV1Mg411A75T/?spm_id_from=333.788.recommend_more_video.19
+
+
+npx eslint index.js 
