892eb4e5d9442d5299e4f85a63a1992ecd64f84f
Aiden <mi_duo@live.com>
2022-09-29 22:39:00 +0800

    feat: react

--- react_demo/hellworld/like_button.js
+++ react_demo/hellworld/like_button.js
@@ -13,12 +13,33 @@ class LikeButton extends React.Component {
         }
         return e(
             'button',
-            {onClick: () => this.setState({liked: true}) },
+            {onClick: () => this.setState({liked: true})},
             'Like');
     }
 }
 
+// class ShoppingList extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {liked: false};
+//     }
+//     render() {
+//         return (
+//             <div className="shopping-list">
+//                 <h1>Shopping List for {this.props.name}</h1>
+//                 <ul>
+//                     <li>Instagram</li>
+//                     <li>WhatsApp</li>
+//                     <li>Oculus</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(<LikeButton name="btn" />);
+//root.render(e(LikeButton));
 
+// root.render(<ShoppingList name="Mark" />);
