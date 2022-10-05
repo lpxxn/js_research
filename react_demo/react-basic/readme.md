524eb684b80955def9344efef0c4ae1b2c700858
Aiden <mi_duo@live.com>
2022-10-05 17:26:31 +0800

    feat: react

--- react_demo/react-basic/readme.md
+++ react_demo/react-basic/readme.md
@@ -7,3 +7,6 @@ npm start
 
 https://www.bilibili.com/video/BV1Z44y1K7Fj?p=5&spm_id_from=pageDriver&vd_source=8d4b220c3958f634b9f3e09271aebf79
 
+jsx并不是标准的JS语法，是JS的语法扩展，浏览器并不认识，脚手架中内置的@babel/tranform-react-jsx插件，用来解析jsx语法。
+
+![jsx转换](img/jsx1.png)
--- react_demo/react-basic/src/App.js
+++ react_demo/react-basic/src/App.js
@@ -1,7 +1,23 @@
+// jsx
+// 1. 识别常规变量
+const name = 'Hello World';
+// 2 原生js方法调用
+const getAge = (_) => 18;
+
+// 3. 三元运算符
+const flag = true;
+
+// 4. 数组
+const arr = [1, 2, 3, 4, 5];
+// if/switch/变量声明，这些是语句，不是表达式，不能直接放在jsx {}中
 
 function App() {
   return (
     <div className="App">
+      <p>{name}</p>
+      { getAge()}
+      { flag ? <p>flag is true</p> : <p>flag is false</p>}
+      { arr.map((item, index) => <p key={index}>{item}</p>)}
       App
     </div>
   );
