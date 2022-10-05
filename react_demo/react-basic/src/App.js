e6678ec8986ad5d10596909cff1c714e1c136047
Aiden <mi_duo@live.com>
2022-10-05 17:46:05 +0800

    feat: react

--- react_demo/react-basic/src/App.js
+++ react_demo/react-basic/src/App.js
@@ -11,13 +11,24 @@ const flag = true;
 const arr = [1, 2, 3, 4, 5];
 // if/switch/变量声明，这些是语句，不是表达式，不能直接放在jsx {}中
 
+// key 只在内部使用，不会出现在真实dom中
+const songs = [
+  {id: 1, name: 'song1'},
+  {id: 2, name: 'song2'},
+  {id: 3, name: 'song3'},
+]
+
 function App() {
   return (
     <div className="App">
       <p>{name}</p>
       { getAge()}
       { flag ? <p>flag is true</p> : <p>flag is false</p>}
-      { arr.map((item, index) => <p key={index}>{item}</p>)}
+      { arr.map((item, index) => <em key={index}>{item}</em>)}
+      {/* { songs.map(song => <p key={song.id}>{song.name}</p>)} */}
+      <ul>
+        { songs.map(song => <li key={song.id}>{song.name}</li>)}
+      </ul>
       App
     </div>
   );
