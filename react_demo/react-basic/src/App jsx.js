99daf968ced7a1e9d53844668da9f13ac845ca5a
Aiden <mi_duo@live.com>
2022-10-06 18:06:48 +0800

    feat: react

--- react_demo/react-basic/src/App jsx.js
+++ react_demo/react-basic/src/App jsx.js
@@ -1,9 +1,10 @@
 // jsx
+// JSX中的语法更加贴近JS语法，属性名采用驼峰命名法  class -> className  for -> htmlFor
 // 1. 识别常规变量
-const name = 'Hello World';
+const name = 'Hello World'
 // 2 原生js方法调用
-const getAge = (_) => 18;
-const getHader =(t) => {
+const getAge = (_) => 18
+const getHader = (t) => {
   if (t === 1) {
     return <h1>h1</h1>
   }
@@ -13,35 +14,35 @@ const getHader =(t) => {
   return <h3>h3</h3>
 }
 // 3. 三元运算符
-const flag = true;
+const flag = true
 
 // 4. 数组
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5]
 // if/switch/变量声明，这些是语句，不是表达式，不能直接放在jsx {}中
 
 // key 只在内部使用，不会出现在真实dom中
 const songs = [
-  {id: 1, name: 'song1'},
-  {id: 2, name: 'song2'},
-  {id: 3, name: 'song3'},
+  { id: 1, name: 'song1' },
+  { id: 2, name: 'song2' },
+  { id: 3, name: 'song3' },
 ]
 
-function App() {
+function App () {
   return (
     <div className="App">
       {getHader(2)}
       <p>{name}</p>
-      { getAge()}
-      { flag ? <p>flag is true</p> : <p>flag is false</p>}
-      {true ? null: <p>flag is false</p>} /*  null 不会渲染 */
-      { arr.map((item, index) => <em key={index}>{item}</em>)}
+      {getAge()}
+      {flag ? <p>flag is true</p> : <p>flag is false</p>}
+      {true ? null : <p>flag is false</p>} /*  null 不会渲染 */
+      {arr.map((item, index) => <em key={index}>{item}</em>)}
       {/* { songs.map(song => <p key={song.id}>{song.name}</p>)} */}
       <ul>
-        { songs.map(song => <li key={song.id}>{song.name}</li>)}
+        {songs.map(song => <li key={song.id}>{song.name}</li>)}
       </ul>
       App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- react_demo/react-basic/src/App.js
+++ react_demo/react-basic/src/App.js
@@ -1,11 +1,11 @@
-import './app.css';
+import './app.css'
 
 // jsx
 // 1. 识别常规变量
-const name = 'Hello World';
+const name = 'Hello World'
 // 2 原生js方法调用
-const getAge = (_) => 18;
-const getHader =(t) => {
+const getAge = (_) => 18
+const getHader = (t) => {
   if (t === 1) {
     return <h1>h1</h1>
   }
@@ -15,17 +15,17 @@ const getHader =(t) => {
   return <h3>h3</h3>
 }
 // 3. 三元运算符
-const flag = true;
+const flag = true
 
 // 4. 数组
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5]
 // if/switch/变量声明，这些是语句，不是表达式，不能直接放在jsx {}中
 
 // key 只在内部使用，不会出现在真实dom中
 const songs = [
-  {id: 1, name: 'song1'},
-  {id: 2, name: 'song2'},
-  {id: 3, name: 'song3'},
+  { id: 1, name: 'song1' },
+  { id: 2, name: 'song2' },
+  { id: 3, name: 'song3' },
 ]
 
 // css stype 
@@ -34,24 +34,24 @@ const stypeObj = {
 }
 // css className eg: className='age'
 
-const showTitle = true;
-function App() {
+const showTitle = true
+function App () {
   return (
     <div className="App">
       {getHader(2)}
       <p style={stypeObj} >{name}</p>
-      <p className='age'>{ getAge()}</p>
-      { flag ? <p>flag is true</p> : <p>flag is false</p>}
+      <p className='age'>{getAge()}</p>
+      {flag ? <p>flag is true</p> : <p>flag is false</p>}
       {/*  null 不会渲染 */}
-      {true ? null: <p>flag is false</p>} 
-      { arr.map((item, index) => <em key={index}>{item}</em>)}
+      {true ? null : <p>flag is false</p>}
+      {arr.map((item, index) => <em key={index}>{item}</em>)}
       {/* { songs.map(song => <p key={song.id}>{song.name}</p>)} */}
       <ul>
-        { songs.map(song => <li key={song.id}>{song.name}</li>)}
+        {songs.map(song => <li key={song.id}>{song.name}</li>)}
       </ul>
-      <div className={showTitle ? 'title': ''}>App</div>
+      <div className={showTitle ? 'title' : ''}>App</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
