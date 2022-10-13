ecfaaab82755837de607be4c9de1d6f9c6c070f0
lpxxn <mi_duo@live.com>
2022-10-14 00:01:53 +0800

    feat: fatcher son

--- react_demo/react-basic/src/FatherSon.js
+++ react_demo/react-basic/src/FatherSon.js
@@ -5,11 +5,14 @@ function Son1 (props) {
   // props 是一个对象，里面存放的是父组件传递过来的数据
   // props 是只读的，不能修改
   // props.name = 'lipeng' //error
-  // props 可以传递任意类型的数据，数字，字符串，对象，数组，函数，JSX
+  // props 可以传递任意类型的数据，数字，字符串，对象，数组，函数，JSX(React元素)
   return (
+    console.log('Son1 props userInfo', props.userInfo),
     <div>
       <h1>son1</h1>
-      <button onClick={props.clickHandler}>点击我{props.message}</button>
+      <button onClick={props.clickHandler}>触发父组件传入的函数{props.list}</button>
+      {props.list.map((item, index) => <label key={index}>{[index, item]}</label>)}
+      {props.userInfo.name}
     </div>
   )
 }
@@ -18,10 +21,12 @@ function Son1 (props) {
 class Son2 extends React.Component {
   render () {
     return (
+      console.log('Son2 props', this.props),
       <div>
         <h1>son2</h1>
         {/* 类组件必须通过this关键词去获取,这里的props是固定的 */}
         <button onClick={() => { this.props.clickHandler2("hello") }}>点击我{this.props.message}</button>
+        {this.props.child}
       </div>
     )
   }
@@ -30,8 +35,16 @@ class Son2 extends React.Component {
 class Father extends React.Component {
   state = {
     message: 'this is father message',
+    list: [1, 2, 3],
+    userInfo: {
+      name: 'zhangsan',
+      age: 18
+    },
     clickHandler: () => {
       console.log('father click', this)
+      this.setState({
+        list: [...this.state.list, 1]
+      })
     },
     clickHandler2: (msg) => {
       console.log('father click2', this, msg)
@@ -48,8 +61,8 @@ class Father extends React.Component {
       <div>
         <h1>父组件</h1>
         <input type="text" value={this.state.message} onChange={this.changeHandler} />
-        <Son1 message={this.state.message} clickHandler={this.state.clickHandler} />
-        <Son2 message={this.state.message} clickHandler2={this.state.clickHandler2}></Son2>
+        <Son1 list={this.state.list} clickHandler={this.state.clickHandler} userInfo={this.state.userInfo} />
+        <Son2 message={this.state.message} clickHandler2={this.state.clickHandler2} child={<span>this is span</span>}></Son2>
       </div>
     )
   }
