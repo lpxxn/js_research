4ffd9674166bf85d5826df02d943d89784060d2f
lpxxn <mi_duo@live.com>
2022-10-13 23:45:09 +0800

    feat: fatcher son

--- react_demo/react-basic/src/FatherSon.js
+++ react_demo/react-basic/src/FatherSon.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-
+// 函数式Son
 function Son1 (props) {
   // props 是一个对象，里面存放的是父组件传递过来的数据
   // props 是只读的，不能修改
@@ -13,11 +13,27 @@ function Son1 (props) {
   )
 }
 
+// 类组件Son
+class Son2 extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>son2</h1>
+        {/* 类组件必须通过this关键词去获取,这里的props是固定的 */}
+        <button onClick={() => { this.props.clickHandler2("hello") }}>点击我{this.props.message}</button>
+      </div>
+    )
+  }
+}
+
 class Father extends React.Component {
   state = {
     message: 'this is father message',
     clickHandler: () => {
       console.log('father click', this)
+    },
+    clickHandler2: (msg) => {
+      console.log('father click2', this, msg)
     }
   }
   changeHandler = (e) => {
@@ -32,6 +48,7 @@ class Father extends React.Component {
         <h1>父组件</h1>
         <input type="text" value={this.state.message} onChange={this.changeHandler} />
         <Son1 message={this.state.message} clickHandler={this.state.clickHandler} />
+        <Son2 message={this.state.message} clickHandler2={this.state.clickHandler2}></Son2>
       </div>
     )
   }
