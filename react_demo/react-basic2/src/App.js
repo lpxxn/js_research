23e4bc45c96ba9a51d87ab23b3801ca4243b6bfa
lpxxn <mi_duo@live.com>
2022-10-16 20:34:47 +0800

    feat: verify propTypes

--- react_demo/react-basic2/src/App.js
+++ react_demo/react-basic2/src/App.js
@@ -1,9 +1,25 @@
 import './App.css'
 
+// 里面有各种各样的内置的校验规则
+import PropTypes from 'prop-types'
+
+function Test ({ list }) {
+  return (
+    <div>
+      {list.map(item => <div key={item}>{item}</div>)}
+    </div>
+  )
+}
+Test.propTypes = {
+  // 自定义校验规则
+  list: PropTypes.array // 限定list必须是数组
+}
+
 function App () {
   return (
     <div className="App">
-
+      hello world
+      <Test list={[1, 2, 3]} />
     </div>
   )
 }
