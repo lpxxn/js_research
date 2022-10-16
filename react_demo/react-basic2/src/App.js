69fff4c82fca694ebb151be9a7c5cac31940992a
lpxxn <mi_duo@live.com>
2022-10-16 21:03:23 +0800

    feat: default propTypes

--- react_demo/react-basic2/src/App.js
+++ react_demo/react-basic2/src/App.js
@@ -2,17 +2,24 @@ import './App.css'
 
 // 里面有各种各样的内置的校验规则
 import PropTypes from 'prop-types'
-
-function Test ({ list }) {
+// 默认值也可以直接写在参数里
+function Test ({ list, pageSize, msg = 'hello' }) {
   return (
     <div>
       {list.map(item => <div key={item}>{item}</div>)}
+      <div>pageSize: {pageSize}</div>
+      <p>msg: {msg}</p>
     </div>
   )
 }
 Test.propTypes = {
   // 自定义校验规则
-  list: PropTypes.array // 限定list必须是数组
+  list: PropTypes.array, // 限定list必须是数组
+}
+
+// 默认值
+Test.defaultProps = {
+  pageSize: 10
 }
 
 function App () {
