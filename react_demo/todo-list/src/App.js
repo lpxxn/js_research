bc8c16399a69c9d60427609ce4c21f493d4b9ce7
lpxxn <mi_duo@live.com>
2022-10-20 21:19:44 +0800

    feat: ant

--- react_demo/todo-list/src/App.js
+++ react_demo/todo-list/src/App.js
@@ -1,12 +1,53 @@
 import './App.css'
-import { Button } from 'antd'
+import React from 'react'
+import { Button, Input } from 'antd'
+const { Search } = Input
 
-function App () {
-  return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    List: [],
+    columns: [
+      {
+        title: '任务编号',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '任务名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '任务描述',
+        dataIndex: 'desc',
+        key: 'desc',
+      },
+      {
+        title: '操作',
+        dataIndex: 'action',
+        key: 'action',
+      }
+
+    ]
+  }
+  // 点击搜索图标、清除图标，或按下回车键时的回调
+  onSearch = (value) => {
+    console.log(value)
+  }
+  render () {
+    return (
+      <div className="container">
+        <Search
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={this.onSearch}
+        />
+      </div>
+    )
+  }
 }
+
 
 export default App
