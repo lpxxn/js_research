525ba63e697fdccd57fc69c24e0187de7fac6bd4
lpxxn <mi_duo@live.com>
2022-10-24 21:49:37 +0800

    feat: hook demo

--- react_demo/hook1/src/App.js
+++ react_demo/hook1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import { useState } from 'react'
 
 function App () {
+  const [count, setCount] = useState(1)
   return (
     <div >
-      hello
+      <button onClick={() => { setCount(count + 1) }}>{count}</button>
     </div>
   )
 }
