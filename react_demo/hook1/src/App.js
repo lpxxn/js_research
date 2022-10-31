3320fee8e79f1cc4c466960b4e2eea2fd0cfbf90
lpxxn <mi_duo@live.com>
2022-11-01 07:18:01 +0800

    feat: hook

--- react_demo/hook1/src/App.js
+++ react_demo/hook1/src/App.js
@@ -9,10 +9,12 @@ import { useState, useEffect } from 'react'
 
 // 重点一句话： useState初始值只在首次渲染生效 后续只要调用setCount整个app中的代码都会执行
 // 此时的count每次拿王老师的都是最新的值
-function App () {
+function App (props) {
   // count: 数据状态
   // setCount: 修改count的函数（专有函数），第一次是0第二次就是计算后的值 count + 1
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    return props.count + 1
+  })
   // useState 只能出现在函数组件中
   // 不能嵌套在if/for其他函数中（react 按照hooks的调用顺序识别每一个hook）
   console.log('app function count: ', count)
