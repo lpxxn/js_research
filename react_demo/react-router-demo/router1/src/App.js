4c05f4ae18effd803a05a6ff6f1eac03542a1075
lpxxn <mi_duo@live.com>
2022-11-02 23:10:55 +0800

    feat: upgrade devices

--- react_demo/react-router-demo/router1/src/App.js
+++ react_demo/react-router-demo/router1/src/App.js
@@ -1,9 +1,19 @@
-import './App.css'
+import Home from './Home'
+import About from './About'
+
+import React from 'react'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
 function App () {
   return (
-    <div >
-    </div>
+    <BrowserRouter>
+      <Link to="/" > 首页 </Link>
+      <Link to="/about" > 关于 </Link>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/about" element={<About></About>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
