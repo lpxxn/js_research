c35e891537de7c3bfbed84d12751e208fcf16c8d
lpxxn <mi_duo@live.com>
2022-11-28 21:35:56 +0800

    er ji route

--- react_demo/pc-demo/src/App.js
+++ react_demo/pc-demo/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Layout from './pages/Layout'
 import { AuthComponment } from './components/AuthComponent'
+import Home from './pages/Home'
+import Article from './pages/Article'
+import Publish from './pages/Publish'
 // 配置路由
 function App () {
   return (
@@ -15,8 +18,15 @@ function App () {
             <AuthComponment>
               <Layout />
             </AuthComponment>
-          } > </Route>
-          <Route path="/login" element={<Login />} > </Route>
+          } >
+            {/* The Home component route does not have a path but has an index attribute. That specifies this route as the default route for the parent route, which is / */}
+            <Route index element={<Home />}></Route>
+            <Route path="article" element={<Article />}></Route>
+            <Route path="publish" element={<Publish />}></Route>
+
+            <Route path="/login" element={<Login />} > </Route>
+          </Route>
+
         </Routes>
       </div>
     </BrowserRouter >
--- react_demo/pc-demo/src/pages/Layout/index.js
+++ react_demo/pc-demo/src/pages/Layout/index.js
@@ -1,20 +1,44 @@
 import React from 'react'
-import './index.css'
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
+import './index.scss'
+import { DiffOutlined, EditOutlined, HomeOutlined, LaptopOutlined, LogoutOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
 import { Breadcrumb, Layout, Menu } from 'antd'
+import { Outlet, Link } from 'react-router-dom'
+import { Popconfirm } from 'antd'
 
 const { Header, Content, Sider } = Layout
 
 const AppLayout = () => {
   return (
     <Layout style={{ height: '100vh' }}>
-      <Header className='header'></Header>
+      <Header className='header'>
+        <div className='logo' >
+          <div className='user-info'>
+            <span className='user-name'>name</span>
+            <span className='user-logout'>
+              <Popconfirm title="是否退出？" okText="退出" cancelText="取消">
+                <LogoutOutlined /> 退出
+              </Popconfirm>
+            </span>
+          </div>
+        </div>
+      </Header>
       <Layout>
         <Sider width={200} className='site-layout-background'>
-          <Menu style={{ height: '100%', borderRight: 0 }}></Menu>
+          <Menu mode='inline' theme='dark' defaultSelectedKeys={['1']}
+            style={{ height: '100%', borderRight: 0 }}>
+            <Menu.Item icon={<HomeOutlined />} key="1">
+              <Link to={'/'}>数据概览</Link>
+            </Menu.Item>
+            <Menu.Item icon={<DiffOutlined />} key="2">
+              <Link to={'/article'}>内容管理</Link>
+            </Menu.Item>
+            <Menu.Item icon={<EditOutlined />} key="3">
+              <Link to='/publish'>编辑文章</Link>
+            </Menu.Item>
+          </Menu>
         </Sider>
         <Layout style={{ padding: '0 2px 2px' }}>
-          <Content>Layout</Content>
+          <Outlet></Outlet>
         </Layout>
       </Layout>
     </Layout>
